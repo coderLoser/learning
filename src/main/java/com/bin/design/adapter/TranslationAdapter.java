2051cbc9230a2b096a1534efa31bbb9a336bd9c6
yuzhibo <zhangshenbinl@163.com>
2020-09-08 22:17:23 +0800

    测试commit

--- src/main/java/com/bin/design/adapter/TranslationAdapter.java
+++ src/main/java/com/bin/design/adapter/TranslationAdapter.java
@@ -1,7 +1,9 @@
 package com.bin.design.adapter;
 
+/**
+ * 适配器，一般来说，我们可以使用 继承 或者 聚合 的形式，来达到使用 AmericaAdaptee API 的目的
+ */
 public class TranslationAdapter extends AmericaAdaptee implements Target{
-    @Override
     public String useCNTalk(String input) {
         System.out.println("将 " + input + " 翻译成英文");
         String eg = useEnglishTalk("english message!");
